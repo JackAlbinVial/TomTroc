db3c1f744f6c243a744704e3c859be10f6f61a59
Jack Vial <jack.albin.vial@gmail.com>
2026-06-25 07:34:43 +0200

    CSS : detail livre

--- views/templates/detailLivre.php
+++ views/templates/detailLivre.php
@@ -3,24 +3,25 @@
  * Template de la page détail d'un livre.
  */
 ?>
-<a href="index.php?action=book">Nos Livres ></a>
-<p><?php echo $livre->getTitre() ?></p>
+<div class="breadcrumb">
+    <a href="index.php?action=book">Nos Livres >&nbsp;</a> <p> <?php echo $livre->getTitre() ?></p>
+</div>
 
-<div>
-    <div class="detailLivreDroit">
-        <img src="./pictures/books/<?php echo $livre->getPhoto(); ?>" style="height:150px;"/>
+<div class="detailLivreContainer">
+    <div class="detailLivreGauche">
+        <img src="./pictures/books/<?php echo $livre->getPhoto(); ?>"/>
     </div>
-    <div class="detailLivregauche">
+    <div class="detailLivreDroit">
         <h1><?php echo $livre->getTitre() ?></h1>
-        <p>par <?php echo $livre->getAuteur() ?></p>
+        <p class="auteur">par <?php echo $livre->getAuteur() ?></p>
         <hr/>
         <h6>DESCRIPTION</h6>
-        <p><?php echo $livre->getDescription() ?></p>
+        <p class="text"><?php echo nl2br($livre->getDescription()) ?></p>
         <h6>PROPRIÉTAIRE</h6>
-        <a href="index.php?action=owner&id=<?php echo $user->getId() ?>">
+        <a href="index.php?action=owner&id=<?php echo $user->getId() ?>" class="proprietaireLink">
             <img src="./pictures/users/<?php echo $user->getPicture(); ?>" alt="Image d'un utilisateur">
             <p><?php echo $user->getName(); ?></p>
         </a>
-        <a href="index.php?action=message&idSender=<?php echo $user->getId() ?>">lololo</a>
+        <a href="index.php?action=message&idSender=<?php echo $user->getId() ?>" class="boutonVert fullWidth">Envoyer un Message</a>
     </div>
 </div>
